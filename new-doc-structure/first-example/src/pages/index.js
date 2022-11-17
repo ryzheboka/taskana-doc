1977e7120e1ee33489e31294e454a5bd2ab81008
knht <43456808+knht@users.noreply.github.com>
2022-11-17 17:51:36 +0100

    Removed unnecessary components and styles

--- new-doc-structure/first-example/src/pages/index.js
+++ new-doc-structure/first-example/src/pages/index.js
@@ -1,37 +1,8 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/user-guide/intro">
-            User Guide
-          </Link>
-        </div>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/contributor-guide/intro">
-            Contributor Guide
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import Hero from '../components/Hero/Hero';
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -39,9 +10,12 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      <Hero />
       <main>
         <HomepageFeatures />
+        <div>
+          <p>More content ...</p>
+        </div>
       </main>
     </Layout>
   );
