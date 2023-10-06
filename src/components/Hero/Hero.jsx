54704c24b10f30e97fd48389c104aad3fbab876b
Elena Mokeeva <elena.mokeeva@envite.de>
2023-10-06 10:17:45 +0200

    Closes #20 - change front page images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,12 +3,15 @@ import React from 'react';
 import Button from '../Button/Button';
 
 const Hero = () => {
+  const h1Style = {
+    color: '#6CCBB2',
+  };
     return (
         <>
             <section className={styles.wrapper}>
                 <section className={styles.hero}>
                     <div className={styles.heroContent}>
-                        <h1>TASKANA</h1>
+                        <h1 style={h1Style}>TASKANA</h1>
                         <p>
                             Some of the functionalities provided by TASKANA are prioritization, distribution of tasks, monitoring the current progress and customization of the task attributes. TASKANA can also help to centralize task management within larger organizations with multiple task management mechanisms.
                             TASKANA is available as a Java library and as a REST API with lots of customization options for your task management system. It can be operated as standalone or be used to build integrated task management into your own application as well.
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,20 +4,39 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Distribution',
-    Svg: require('@site/static/img/distribution.png').default,
+    title: 'Workbasket',
+    Svg: require('@site/static/img/WorkbasketNew.png').default,
     description: (
       <>
-        TASKANA can be configured to distribute tasks from one workbasket to multiple others.
+        A Workbasket is a list of Tasks. Each Task is placed into exactly one Workbasket. You can specify access rights and other attributes of a Workbasket.
+      </>
+    ),
+  },
+  {
+    title: 'Routing',
+    Svg: require('@site/static/img/routing.png').default,
+    description: (
+      <>
+        TASKANA can route Tasks into different Workbaskets. The routing is performed automatically according to customized rules.
       </>
     ),
   },
   {
     title: 'Monitoring',
-    Svg: require('@site/static/img/Monitoring.png').default,
+    Svg: require('@site/static/img/monitoring-logo.png').default,
     description: (
       <>
-        You can monitor the current state of work including tasks, their states, their due dates etc..
+        TASKANA visualizes the current state of work. This includes Tasks, their states, their due dates etc.. For example, you can view the number of due Tasks plotted by time.
+      </>
+    ),
+  },
+
+  {
+    title: 'Prioritization, Sorting & Filtering',
+    Svg: require('@site/static/img/PriorityNew.png').default,
+    description: (
+      <>
+        Tasks can be prioritized automatically or manually. TASKANA also provides sorting and filtering by priority or by other attributes.
       </>
     ),
   }
@@ -25,15 +44,17 @@ const FeatureList = [
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--6')}>
+    <div className={clsx('col col--3')}>
       <div className="text--center">
         <img className={styles.featureSvg} src={Svg} />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="text--center padding-horiz--lg">
         <h3>{title}</h3>
-        <p>{description}</p>
+        <div className={styles.featureText}>
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
 
